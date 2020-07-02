220ddff500058f3802871a52850b5e79c0d086a3
chanuko <chanu@gontoh.net>
2020-07-02 15:26:24 +0900

    日本語消した

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import Adafruit_CharLCD as LCD
 from datetime import datetime
 import time
 
-# ピンの設定. GPIO番号を入れる　
 lcd_rs = 22
 lcd_en = 26
 lcd_d4 = 5
@@ -10,9 +9,9 @@ lcd_d5 = 6
 lcd_d6 = 13
 lcd_d7 = 19
 
-# 16x2桁
+# 16x2
 lcd_columns = 16
-lcd_rows    = 2
+lcd_rows = 2
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
 
 while(1):
@@ -20,5 +19,5 @@ while(1):
     current_date = current_date.strftime('%m-%d')
     current_time = current_date.strftime('%H:%M:%S')
     lcd.clear()
-    lcd.message('Hello World!16x2\nQiita.com/mt08/')
+    lcd.message(current_date ' ' + current_time)
     time.sleep(1)
