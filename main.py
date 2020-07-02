5bb959c9a22831961b3a77f3d163763f84faaea4
chanuko <chanu@gontoh.net>
2020-07-02 15:28:00 +0900

    凡ミスを修正

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_c
 
 while(1):
     current_dt = datetime.now()
-    current_date = current_date.strftime('%m-%d')
-    current_time = current_date.strftime('%H:%M:%S')
+    current_date = current_dt.strftime('%m-%d')
+    current_time = current_dt.strftime('%H:%M:%S')
     lcd.clear()
     lcd.message(current_date + ' ' + current_time)
     time.sleep(1)
